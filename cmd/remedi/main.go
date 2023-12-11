1bcdf9eee1dbfe3bda5e2a77a200d83482502d52
Roman Shelekhov <shelekhoff@gmail.com>
2023-12-12 00:08:28 +0400

    updated storage path

--- cmd/remedi/main.go
+++ cmd/remedi/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rshelekhov/remedi/internal/config"
 	"github.com/rshelekhov/remedi/internal/lib/logger/sl"
+	"github.com/rshelekhov/remedi/internal/storage/postgres"
 	"log/slog"
+	"os"
 )
 
 func main() {
@@ -14,6 +17,28 @@ func main() {
 	// A field with information about the current environment will be added to each message
 	log = log.With(slog.String("env", cfg.Env))
 
-	log.Info("initializing server", slog.String("address", cfg.Address))
+	log.Info(
+		"initializing server",
+		slog.String("address", cfg.HTTPServer.Address))
 	log.Debug("logger debug mode enabled")
+
+	storage, err := postgres.NewStorage(
+		fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			cfg.Postgres.Host,
+			cfg.Postgres.Port,
+			cfg.Postgres.User,
+			cfg.Postgres.Password,
+			cfg.Postgres.DBName,
+			cfg.Postgres.SSLMode))
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+	}
+
+	defer func(storage *postgres.Storage) {
+		err := storage.Close()
+		if err != nil {
+			log.Error("failed to close storage", err)
+			os.Exit(1)
+		}
+	}(storage)
 }
