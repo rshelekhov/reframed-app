0ddc6bbbcaf96a4903db232f7aa522ae1d413fc9
Roman Shelekhov <shelekhoff@gmail.com>
2023-12-12 00:09:04 +0400

    add migrator cli utility

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,1 +1,54 @@
-package migrator
+package main
+
+//
+// A small CLI utility for running database migrations
+//
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/rshelekhov/remedi/internal/config"
+)
+
+func main() {
+	var migrationsPath, migrationsTable string
+
+	cfg := config.MustLoad()
+
+	flag.StringVar(&migrationsPath, "migrations-path", "./migrations", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", cfg.Postgres.DBTestName, "name of database for functional tests")
+	flag.Parse()
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		// postgres://user:password@host:port/dbname?sslmode=disable
+		fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+			cfg.Postgres.User,
+			cfg.Postgres.Password,
+			cfg.Postgres.Host,
+			cfg.Postgres.Port,
+			cfg.Postgres.DBName,
+			cfg.Postgres.SSLMode),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+}
