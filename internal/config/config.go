8849c0851f2e16e63aa90672c0e730209aec622d
Roman Shelekhov <shelekhoff@gmail.com>
2023-12-12 19:20:16 +0400

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,7 @@ import (
 type Config struct {
 	Env        string           `yaml:"env" env-default:"development"`
 	HTTPServer HTTPServerConfig `yaml:"http_server"`
-	Postgres   PostgresConfig   `yaml:"postgres" env-required:"true"`
+	Database   DatabaseConfig   `yaml:"database" env-required:"true"`
 }
 
 type HTTPServerConfig struct {
@@ -19,7 +19,7 @@ type HTTPServerConfig struct {
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
-type PostgresConfig struct {
+type DatabaseConfig struct {
 	DBName   string `yaml:"db_name"`
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
