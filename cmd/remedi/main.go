b33248026f17027426b8a077ff25dcbaf99abce2
Roman Shelekhov <shelekhoff@gmail.com>
2023-12-21 00:37:17 +0400

    add http-server

--- cmd/remedi/main.go
+++ cmd/remedi/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/rshelekhov/remedi/api/router"
 	"github.com/rshelekhov/remedi/internal/config"
 	"github.com/rshelekhov/remedi/internal/storage/postgres"
 	"github.com/rshelekhov/remedi/internal/util/logger/sl"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -28,10 +33,44 @@ func main() {
 		log.Error("failed to init storage", sl.Err(err))
 	}
 	log.Debug("storage initiated")
-	
+
 	r := router.New(log)
 
-	// TODO: remove this
+	log.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      r,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	// TODO: move timeout to config
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	// TODO: remove this (?)
 	defer func(storage *postgres.Storage) {
 		err := storage.Close()
 		if err != nil {
