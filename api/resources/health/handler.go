953c9815542ce0d12cb45c2bdf0ef38562fa39af
Roman Shelekhov <shelekhoff@gmail.com>
2023-12-21 15:00:49 +0400

    add healthcheck handler

--- api/resources/health/handler.go
+++ api/resources/health/handler.go
@@ -1,1 +1,17 @@
 package health
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func RegisterHandlers(r *chi.Mux) {
+	r.Get("/health", health)
+}
+
+func health(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		return
+	}
+}
--- api/router/router.go
+++ api/router/router.go
@@ -4,33 +4,37 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
+	"github.com/rshelekhov/remedi/api/resources/health"
 	mwlogger "github.com/rshelekhov/remedi/internal/http-server/middleware/logger"
 	"log/slog"
 )
 
 func New(log *slog.Logger) *chi.Mux {
-	router := chi.NewRouter()
+	r := chi.NewRouter()
 
 	// Add request_id to each request, for tracing purposes
-	router.Use(middleware.RequestID)
+	r.Use(middleware.RequestID)
 
 	// Logging of all requests
-	router.Use(middleware.Logger)
+	r.Use(middleware.Logger)
 
 	// By default, middleware.Logger uses its own internal logger,
 	// which should be overridden to use ours. Otherwise, problems
 	// may arise - for example, with log collection. We can use
 	// our own middleware to log requests:
-	router.Use(mwlogger.New(log))
+	r.Use(mwlogger.New(log))
 
 	// If a panic happens somewhere inside the server (request handler),
 	// the application should not crash.
-	router.Use(middleware.Recoverer)
+	r.Use(middleware.Recoverer)
 
 	// Parser of incoming request URLs
-	router.Use(middleware.URLFormat)
+	r.Use(middleware.URLFormat)
 
-	router.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-	return router
+	// r.Get("/health", health.Read)
+	health.RegisterHandlers(r)
+
+	return r
 }
