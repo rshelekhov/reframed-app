9239682656c6ea3e63cdce85e8b9b77ce6a147d2
Roman Shelekhov <shelekhoff@gmail.com>
2024-01-22 23:14:18 +0400

    update migration files

--- migrations/000002_zero_dump.up.sql
+++ migrations/000002_zero_dump.up.sql
@@ -1,16 +1,3 @@
-INSERT INTO goal_statuses (id, status_name) VALUES
-    (1, 'Not started'),
-    (2, 'On hold'),
-    (3, 'In progress'),
-    (4, 'Achieved');
-
-INSERT INTO action_statuses (id, status_name) VALUES
+INSERT INTO statuses (id, status_name) VALUES
     (1, 'Not started'),
     (2, 'Completed');
-
-INSERT INTO priorities (id, priority_name) VALUES
-    (1, 'No priority'),
-    (2, 'Urgent'),
-    (3, 'High'),
-    (4, 'Medium'),
-    (5, 'Low');
