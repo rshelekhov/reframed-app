c19cb92004e7fcbbc2c35c212b9352284f25d736
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-20 11:20:12 +0400

    added tests with fail cases for refresh-tokens method

--- api_tests/refresh_tokens_test.go
+++ api_tests/refresh_tokens_test.go
@@ -3,14 +3,15 @@ package api_tests
 import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/segmentio/ksuid"
 	"net/http"
 	"net/url"
 	"testing"
 )
 
 func TestRefreshTokenUsingCookie_HappyPath(t *testing.T) {
-
 	u := url.URL{
 		Scheme: "http",
 		Host:   host,
@@ -26,23 +27,22 @@ func TestRefreshTokenUsingCookie_HappyPath(t *testing.T) {
 		}).
 		Expect().
 		Status(http.StatusCreated).
-		Cookie("refreshToken")
+		Cookie(jwtoken.RefreshTokenKey)
 
 	var refreshToken string
 	tokenData.Value().Decode(&refreshToken)
 
 	// Refresh tokens using cookies and check if access token is returned
 	c := e.POST("/refresh-tokens").
-		WithCookie("refreshToken", refreshToken).
+		WithCookie(jwtoken.RefreshTokenKey, refreshToken).
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object()
 
-	c.Value("accessToken").String().NotEmpty()
+	c.Value(jwtoken.AccessTokenKey).String().NotEmpty()
 }
 
 func TestRefreshTokenUsingHeader_HappyPath(t *testing.T) {
-
 	u := url.URL{
 		Scheme: "http",
 		Host:   host,
@@ -58,17 +58,74 @@ func TestRefreshTokenUsingHeader_HappyPath(t *testing.T) {
 		}).
 		Expect().
 		Status(http.StatusCreated).
-		Cookie("refreshToken")
+		Cookie(jwtoken.RefreshTokenKey)
 
 	var refreshToken string
 	tokenData.Value().Decode(&refreshToken)
 
 	// Refresh tokens using HTTP header and check if access token is returned
 	h := e.POST("/refresh-tokens").
-		WithHeader("refreshToken", refreshToken).
+		WithHeader(jwtoken.RefreshTokenKey, refreshToken).
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object()
 
-	h.Value("accessToken").String().NotEmpty()
+	h.Value(jwtoken.AccessTokenKey).String().NotEmpty()
+}
+
+func TestRefreshToken_FailCases(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+			AppID:    appID,
+		}).
+		Expect().
+		Status(http.StatusCreated)
+
+	testCases := []struct {
+		name         string
+		appID        int32
+		refreshToken string
+		status       int
+	}{
+		{
+			name:         "Refresh with empty refresh token",
+			appID:        appID,
+			refreshToken: "",
+			status:       http.StatusUnauthorized,
+		},
+		{
+			name:         "Refresh when session not found",
+			appID:        appID,
+			refreshToken: ksuid.New().String(),
+			status:       http.StatusUnauthorized,
+		},
+		//
+		// This test case is actual if we get appID from the request (from the client)
+		//
+		//{
+		//	name:         "Refresh with empty app id",
+		//	appID:        emptyAppID,
+		//	refreshToken: refreshToken,
+		//	status:       http.StatusBadRequest,
+		//},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Refresh tokens using HTTP header
+			e.POST("/refresh-tokens").
+				WithCookie(jwtoken.RefreshTokenKey, tc.refreshToken).
+				Expect().
+				Status(tc.status)
+		})
+	}
 }
