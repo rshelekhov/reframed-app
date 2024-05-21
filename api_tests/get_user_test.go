19e4c066ae6b4ce014eac87da2af02a20439e99a
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-21 11:01:21 +0400

    added test for get user method

--- api_tests/get_user_test.go
+++ api_tests/get_user_test.go
@@ -1,1 +1,39 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetUser_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	resp := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+			AppID:    appID,
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := resp.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Get user
+	e.GET("/user/").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+}
