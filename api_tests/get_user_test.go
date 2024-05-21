1a2c4c92b39c496b435dc1cb2e4f46d7c1abc756
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-21 11:24:37 +0400

    added tests with fail cases for get user method

--- api_tests/get_user_test.go
+++ api_tests/get_user_test.go
@@ -37,3 +37,54 @@ func TestGetUser_HappyPath(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object().NotEmpty()
 }
+
+func TestGetUser_FailCases(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+			AppID:    appID,
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	testCases := []struct {
+		name        string
+		accessToken string
+		status      int
+	}{
+		{
+			name:        "Get user with empty access token",
+			accessToken: "",
+			status:      http.StatusUnauthorized,
+		},
+		{
+			name:        "Get user with invalid access token",
+			accessToken: "invalid",
+			status:      http.StatusUnauthorized,
+		},
+	}
+
+	for _, tc := range testCases {
+		e.GET("/user/").
+			WithHeader("Authorization", "Bearer "+tc.accessToken).
+			Expect().
+			Status(tc.status)
+	}
+}
+
+// TODO: add this test
+// Register user and save access token to variable
+// Delete user
+// Try to get deleted user
+
+//func TestGetUserNotFound(t *testing.T) {
+//}
