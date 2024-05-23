5df86a30d2aa386cbdbb50cebbe475c3a991e8a6
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-23 22:18:49 +0400

    added test for get list by id method

--- api_tests/get_list_test.go
+++ api_tests/get_list_test.go
@@ -1,1 +1,56 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetListByID_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+	t.Log(accessToken)
+
+	// Create list
+	c := e.POST("/user/lists/").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.ListRequestData{
+			Title: gofakeit.Word(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	listID := c.Value("data").Object().Value("id").String().Raw()
+
+	t.Log(listID)
+
+	// Get list by ID
+	getListResp := e.GET("/user/lists/{list_id}").
+		WithPath("list_id", listID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+
+	t.Log(getListResp.Raw())
+}
