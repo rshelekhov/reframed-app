bc857fea0554abfa1f8a515c2876ef65cd15673d
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-30 23:22:24 +0400

    removed unused structs

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,19 +1,8 @@
 package model
 
-import (
-	"time"
-)
+import "time"
 
-// User DB model
 type (
-	User struct {
-		ID           string    `db:"id"`
-		Email        string    `db:"email"`
-		PasswordHash string    `db:"password_hash"`
-		UpdatedAt    time.Time `db:"updated_at"`
-		DeletedAt    time.Time `db:"deleted_at"`
-	}
-
 	UserRequestData struct {
 		Email           string `json:"email" validate:"required,email"`
 		Password        string `json:"password" validate:"required,min=8"`
