aad3543159e68bfe0d9517a01cdeef55a07ed921
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-30 14:28:44 +0400

    added test with happy path for CreateTaskInDefaultList method

--- api_tests/create_task_test.go
+++ api_tests/create_task_test.go
@@ -1,1 +1,39 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestCreateTaskInDefaultList_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Create task
+	e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(randomFakeTask(true, true, true, true, true, "", "")).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+}
