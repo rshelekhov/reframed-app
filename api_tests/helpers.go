441c68c5e8b36ca4ba29a94cbc8c59a0ab54478b
Roman Shelekhov <shelekhoff@gmail.com>
2024-05-31 13:37:41 +0400

    updated methods for generating fake tasks

--- api_tests/helpers.go
+++ api_tests/helpers.go
@@ -21,11 +21,57 @@ func randomFakePassword() string {
 	return gofakeit.Password(true, true, true, true, true, passwordDefaultLength)
 }
 
-func randomTimeRange(isSet bool, start, end time.Time) time.Time {
-	if isSet {
-		return gofakeit.DateRange(start, end)
+func randomFakeTask(isStartDate, isDeadline, isStartTime, isEndTime, isTags bool, listID, headingID string) model.TaskRequestData {
+	startDate := randomDateRange(isStartDate, time.Now(), time.Now().AddDate(0, 0, 1))
+	startTime := randomDateTimeRange(isStartTime, time.Now(), time.Now().AddDate(0, 0, 1))
+	var deadline, endTime string
+
+	if isDeadline {
+		startDateParsed, _ := time.Parse(time.DateOnly, startDate)
+		deadline = randomDateRange(isDeadline, startDateParsed, startDateParsed.Add(randomDays()))
 	}
-	return time.Time{}
+	if isEndTime {
+		startTimeParsed, _ := time.Parse(time.DateTime, startTime)
+		endTime = randomDateTimeRange(isStartTime, startTimeParsed, startTimeParsed.Add(randomTimeDuration()))
+	}
+
+	return model.TaskRequestData{
+		Title:       gofakeit.Sentence(titleDefaultLength),
+		Description: gofakeit.Paragraph(paragraphDefaultCount, sentenceDefaultCount, wordDefaultCount, paragraphDefaultSeparator),
+		StartDate:   startDate,
+		Deadline:    deadline,
+		StartTime:   startTime,
+		EndTime:     endTime,
+		ListID:      listID,
+		HeadingID:   headingID,
+		Tags:        randomTags(isTags),
+	}
+}
+
+func randomDateRange(isSet bool, start, end time.Time) string {
+	if isSet {
+		date := gofakeit.DateRange(start, end)
+		date = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, time.UTC)
+		return date.Format(time.DateOnly)
+	}
+	return ""
+}
+
+func randomDateTimeRange(isSet bool, start, end time.Time) string {
+	if isSet {
+		return gofakeit.DateRange(start, end).Format(time.DateTime)
+	}
+	return ""
+}
+
+func randomDays() time.Duration {
+	return time.Duration(rand.Int63n(30)) * (24 * time.Hour)
+}
+
+func randomTimeDuration() time.Duration {
+	min := rand.Intn(3600)
+	max := rand.Intn(3600) + 3600
+	return time.Duration(rand.Int63n(int64(max-min))+int64(min)) * time.Second
 }
 
 func randomTags(isSet bool) []string {
@@ -38,18 +84,4 @@ func randomTags(isSet bool) []string {
 		return tags
 	}
 	return nil
-}
-
-func randomFakeTask(isStartDate, isDeadline, isStartTime, isEndTime, isTags bool, listID, headingID string) model.TaskRequestData {
-	return model.TaskRequestData{
-		Title:       gofakeit.Sentence(titleDefaultLength),
-		Description: gofakeit.Paragraph(paragraphDefaultCount, sentenceDefaultCount, wordDefaultCount, paragraphDefaultSeparator),
-		StartDate:   randomTimeRange(isStartDate, time.Now(), time.Now().AddDate(0, 0, 1)),
-		Deadline:    randomTimeRange(isDeadline, time.Now(), time.Now().AddDate(0, 0, 1)),
-		StartTime:   randomTimeRange(isStartTime, time.Now(), time.Now().AddDate(0, 0, 1)),
-		EndTime:     randomTimeRange(isEndTime, time.Now(), time.Now().AddDate(0, 0, 1)),
-		ListID:      listID,
-		HeadingID:   headingID,
-		Tags:        randomTags(isTags),
-	}
 }
