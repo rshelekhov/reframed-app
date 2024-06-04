a5594711971a910bdd24005376e5d26e69be3f21
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-04 13:45:31 +0400

    added test for checking if user try to delete default list

--- api_tests/delete_list_test.go
+++ api_tests/delete_list_test.go
@@ -48,3 +48,40 @@ func TestDeleteList_HappyPath(t *testing.T) {
 		Expect().
 		Status(http.StatusOK)
 }
+
+func TestDeleteDefaultList_BadRequest(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Get default list
+	l := e.GET("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	defaultListID := l.Value("data").Object().Value("list_id").String().Raw()
+
+	// Remove default list
+	e.DELETE("/user/lists/{list_id}").
+		WithPath("list_id", defaultListID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusBadRequest)
+
+}
