42d34163bbec3ff08ffe60a3928355ca370090d9
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-04 13:46:38 +0400

    remove test CreateTaskInDefaultList_ListNotFound

--- api_tests/create_task_test.go
+++ api_tests/create_task_test.go
@@ -41,51 +41,6 @@ func TestCreateTaskInDefaultList_HappyPath(t *testing.T) {
 		JSON().Object()
 }
 
-func TestCreateTaskInDefaultList_ListNotFound(t *testing.T) {
-	u := url.URL{
-		Scheme: "http",
-		Host:   host,
-	}
-	e := httpexpect.Default(t, u.String())
-
-	// Register user
-	r := e.POST("/register").
-		WithJSON(model.UserRequestData{
-			Email:    gofakeit.Email(),
-			Password: randomFakePassword(),
-		}).
-		Expect().
-		Status(http.StatusCreated).
-		JSON().Object()
-
-	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
-
-	// Get default list
-	l := e.GET("/user/lists/default").
-		WithHeader("Authorization", "Bearer "+accessToken).
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object()
-
-	defaultListID := l.Value("data").Object().Value("list_id").String().Raw()
-
-	// Remove default list
-	e.DELETE("/user/lists/{list_id}").
-		WithPath("list_id", defaultListID).
-		WithHeader("Authorization", "Bearer "+accessToken).
-		Expect().
-		Status(http.StatusOK)
-
-	fakeTask := randomFakeTask(false, false, false, false, false, "", "")
-
-	// Create task
-	e.POST("/user/lists/default").
-		WithHeader("Authorization", "Bearer "+accessToken).
-		WithJSON(fakeTask).
-		Expect().
-		Status(http.StatusNotFound)
-}
-
 func TestCreateTaskOnSpecificList_HappyPath(t *testing.T) {
 	u := url.URL{
 		Scheme: "http",
@@ -143,4 +98,68 @@ func TestCreateTaskOnSpecificList_HappyPath(t *testing.T) {
 	taskList := getTask.Value("data").Object().Value("list_id").String().Raw()
 
 	require.Equal(t, listID, taskList)
+}
+
+func TestCreateTaskOnSpecificHeading_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Create list
+	l := e.POST("/user/lists/").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.ListRequestData{
+			Title: gofakeit.Word(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	listID := l.Value("data").Object().Value("list_id").String().Raw()
+
+	// Create heading
+	h := e.POST("/user/lists/{list_id}/headings/").
+		WithPath("list_id", listID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.HeadingRequestData{
+			Title:  gofakeit.Word(),
+			ListID: listID,
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	headingID := h.Value("data").Object().Value("heading_id").String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	task := e.POST("/user/lists/{list_id}/headings/{heading_id}/").
+		WithPath("list_id", listID).
+		WithPath("heading_id", headingID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	task.NotEmpty()
+
+	taskHeadingID := task.Value("data").Object().Value("heading_id").String().Raw()
+
+	require.Equal(t, headingID, taskHeadingID)
 }
