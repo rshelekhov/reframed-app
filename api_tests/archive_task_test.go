a5426d656234e2cee2b4c00db7bea5246fd708a4
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-05 23:58:03 +0400

    added test with happy path for ArchiveTask handler

--- api_tests/archive_task_test.go
+++ api_tests/archive_task_test.go
@@ -1,1 +1,50 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestArchiveTask_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	task := e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	taskID := task.Value("data").Object().Value("task_id").String().Raw()
+
+	// Archive task
+	e.PATCH("/user/tasks/{task_id}/archive", taskID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+}
