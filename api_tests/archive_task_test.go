f78f483548610cbbc5c0bb75784dc021a15a8620
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-06 00:04:31 +0400

    added test case when task not found

--- api_tests/archive_task_test.go
+++ api_tests/archive_task_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gavv/httpexpect/v2"
 	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/segmentio/ksuid"
 	"net/http"
 	"net/url"
 	"testing"
@@ -40,6 +41,35 @@ func TestArchiveTask_HappyPath(t *testing.T) {
 		JSON().Object()
 
 	taskID := task.Value("data").Object().Value("task_id").String().Raw()
+
+	// Archive task
+	e.PATCH("/user/tasks/{task_id}/archive", taskID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+}
+
+func TestArchiveTask_NotFound(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	taskID := ksuid.New().String()
 
 	// Archive task
 	e.PATCH("/user/tasks/{task_id}/archive", taskID).
