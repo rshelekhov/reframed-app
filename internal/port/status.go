db3be086157d3b6eb5e9e386da51cf19fc87e017
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-11 00:24:58 +0400

    added methods for StatusUsecase interface

--- internal/port/status.go
+++ internal/port/status.go
@@ -1,1 +1,17 @@
 package port
+
+import (
+	"context"
+	"github.com/rshelekhov/reframed/internal/model"
+)
+
+type (
+	StatusUsecase interface {
+		GetStatuses(ctx context.Context) ([]model.StatusResponseData, error)
+		GetStatusID(ctx context.Context, statusName string) (int, error)
+		GetStatusName(ctx context.Context, statusID int) (string, error)
+	}
+
+	StatusStorage interface {
+	}
+)
