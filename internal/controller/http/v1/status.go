bb96834db61f94b7d2c4f71aaa21dbf09630d5cc
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-11 00:17:30 +0400

    added routes for getting statuses

--- internal/controller/http/v1/status.go
+++ internal/controller/http/v1/status.go
@@ -1,1 +1,41 @@
 package v1
+
+import (
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/port"
+	"log/slog"
+	"net/http"
+)
+
+type statusController struct {
+	logger  *slog.Logger
+	jwt     *jwtoken.TokenService
+	usecase port.StatusUsecase
+}
+
+func newStatusController(
+	log *slog.Logger,
+	jwt *jwtoken.TokenService,
+	usecase port.StatusUsecase,
+) *statusController {
+	return &statusController{
+		logger:  log,
+		jwt:     jwt,
+		usecase: usecase,
+	}
+}
+
+func (c *statusController) GetStatuses() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
+func (c *statusController) GetStatusID() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
+func (c *statusController) GetStatusName() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
