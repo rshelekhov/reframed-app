84e977aab194fa4d36029e157e8a9ec798f24c97
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-11 23:43:59 +0400

    added test with fail cases

--- api_tests/archive_task_test.go
+++ api_tests/archive_task_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
 	"github.com/segmentio/ksuid"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/url"
 	"testing"
@@ -43,14 +44,27 @@ func TestArchiveTask_HappyPath(t *testing.T) {
 	taskID := task.Value("data").Object().Value("task_id").String().Raw()
 
 	// Archive task
-	e.PATCH("/user/tasks/{task_id}/archive", taskID).
+	archivedTask := e.PATCH("/user/tasks/{task_id}/archive", taskID).
 		WithHeader("Authorization", "Bearer "+accessToken).
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object().NotEmpty()
+
+	taskStatusID := archivedTask.Value("data").Object().Value("status_id").Raw()
+
+	// Get status
+	taskStatus := e.GET("/statuses/{status_id}", taskStatusID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	taskStatusTitle := taskStatus.Value("data").Object().Value("title").String().Raw()
+
+	require.Equal(t, taskStatusTitle, model.StatusArchived.String())
 }
 
-func TestArchiveTask_NotFound(t *testing.T) {
+func TestArchiveTask_FailCases(t *testing.T) {
 	u := url.URL{
 		Scheme: "http",
 		Host:   host,
@@ -69,12 +83,29 @@ func TestArchiveTask_NotFound(t *testing.T) {
 
 	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
 
-	taskID := ksuid.New().String()
+	testCases := []struct {
+		name   string
+		taskID string
+		status int
+	}{
+		{
+			name:   "Archive task with empty task_id",
+			taskID: "",
+			status: http.StatusBadRequest,
+		},
+		{
+			name:   "Archive task when task not found",
+			taskID: ksuid.New().String(),
+			status: http.StatusNotFound,
+		},
+	}
 
-	// Archive task
-	e.PATCH("/user/tasks/{task_id}/archive", taskID).
-		WithHeader("Authorization", "Bearer "+accessToken).
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object().NotEmpty()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			e.PATCH("/user/tasks/{task_id}/archive", tc.taskID).
+				WithHeader("Authorization", "Bearer "+accessToken).
+				Expect().
+				Status(tc.status)
+		})
+	}
 }
