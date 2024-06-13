d14bbc244a230919524f2b8c6e39e9c008928d70
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-13 23:27:33 +0400

    added test with happy path

---
+++ api_tests/update_task_time_test.go
@@ -0,0 +1,57 @@
+package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestUpdateTaskTime_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	task := e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	taskID := task.Value("data").Object().Value("task_id").String().Raw()
+
+	startTime, endTime := randomTimeInterval()
+
+	// Update task time
+	e.PATCH("/user/tasks/{task_id}/time").
+		WithPath("task_id", taskID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.TaskRequestTimeData{
+			StartTime: startTime,
+			EndTime:   endTime,
+		}).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+}
