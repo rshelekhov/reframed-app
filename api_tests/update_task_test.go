3f58639f81d042f3e45daa7695e19e0b5da59bdc
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-14 18:55:42 +0400

    refactored tests, added test with case when task not found

--- api_tests/update_task_test.go
+++ api_tests/update_task_test.go
@@ -3,8 +3,10 @@ package api_tests
 import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/constants/key"
 	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/segmentio/ksuid"
 	"net/http"
 	"net/url"
 	"testing"
@@ -12,7 +14,7 @@ import (
 
 func TestUpdateTask_HappyPath(t *testing.T) {
 	u := url.URL{
-		Scheme: "http",
+		Scheme: scheme,
 		Host:   host,
 	}
 	e := httpexpect.Default(t, u.String())
@@ -39,11 +41,10 @@ func TestUpdateTask_HappyPath(t *testing.T) {
 		Status(http.StatusCreated).
 		JSON().Object()
 
-	taskID := task.Value("data").Object().Value("task_id").String().Raw()
+	taskID := task.Value(key.Data).Object().Value(key.TaskID).String().Raw()
 
 	// Update task
-	e.PATCH("/user/tasks/{task_id}").
-		WithPath("task_id", taskID).
+	e.PATCH("/user/tasks/{task_id}", taskID).
 		WithHeader("Authorization", "Bearer "+accessToken).
 		WithJSON(model.TaskRequestData{
 			Title: gofakeit.Word(),
@@ -51,4 +52,45 @@ func TestUpdateTask_HappyPath(t *testing.T) {
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object().NotEmpty()
+}
+
+func TestUpdateTask_NotFound(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object().NotEmpty()
+
+	fakeID := ksuid.New().String()
+
+	// Update task
+	e.PATCH("/user/tasks/{task_id}", fakeID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.TaskRequestData{
+			Title: gofakeit.Word(),
+		}).
+		Expect().
+		Status(http.StatusNotFound)
 }
