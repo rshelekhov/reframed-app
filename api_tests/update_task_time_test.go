e4dc2f3240ac275deea692929aa94999a06113af
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-14 18:54:08 +0400

    refactored test, added test with fail cases

--- api_tests/update_task_time_test.go
+++ api_tests/update_task_time_test.go
@@ -3,8 +3,10 @@ package api_tests
 import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/constants/key"
 	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/segmentio/ksuid"
 	"net/http"
 	"net/url"
 	"testing"
@@ -12,7 +14,7 @@ import (
 
 func TestUpdateTaskTime_HappyPath(t *testing.T) {
 	u := url.URL{
-		Scheme: "http",
+		Scheme: scheme,
 		Host:   host,
 	}
 	e := httpexpect.Default(t, u.String())
@@ -39,13 +41,12 @@ func TestUpdateTaskTime_HappyPath(t *testing.T) {
 		Status(http.StatusCreated).
 		JSON().Object()
 
-	taskID := task.Value("data").Object().Value("task_id").String().Raw()
+	taskID := task.Value(key.Data).Object().Value(key.TaskID).String().Raw()
 
 	startTime, endTime := randomTimeInterval()
 
 	// Update task time
-	e.PATCH("/user/tasks/{task_id}/time").
-		WithPath("task_id", taskID).
+	e.PATCH("/user/tasks/{task_id}/time", taskID).
 		WithHeader("Authorization", "Bearer "+accessToken).
 		WithJSON(model.TaskRequestTimeData{
 			StartTime: startTime,
@@ -54,4 +55,82 @@ func TestUpdateTaskTime_HappyPath(t *testing.T) {
 		Expect().
 		Status(http.StatusOK).
 		JSON().Object().NotEmpty()
+}
+
+func TestUpdateTaskTime_FailCases(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	task := e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	taskID := task.Value(key.Data).Object().Value(key.TaskID).String().Raw()
+
+	startTime, endTime := randomTimeInterval()
+
+	testCases := []struct {
+		name      string
+		taskID    string
+		startTime string
+		endTime   string
+		status    int
+	}{
+		{
+			name:      "Update task time with empty start time",
+			taskID:    taskID,
+			startTime: "",
+			endTime:   endTime,
+			status:    http.StatusBadRequest,
+		},
+		{
+			name:      "Update task time with empty end time",
+			taskID:    taskID,
+			startTime: startTime,
+			endTime:   "",
+			status:    http.StatusBadRequest,
+		},
+		{
+			name:      "Update task time when task not found",
+			taskID:    ksuid.New().String(),
+			startTime: startTime,
+			endTime:   endTime,
+			status:    http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Update task time
+			e.PATCH("/user/tasks/{task_id}/time", tc.taskID).
+				WithHeader("Authorization", "Bearer "+accessToken).
+				WithJSON(model.TaskRequestTimeData{
+					StartTime: tc.startTime,
+					EndTime:   tc.endTime,
+				}).
+				Expect().
+				Status(tc.status)
+		})
+	}
 }
