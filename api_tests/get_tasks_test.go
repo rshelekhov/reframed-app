a35dc20af3e38295d9606cf2b464a4ccc40ab496
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-14 18:34:18 +0400

    added tests for GetTasks handler

--- api_tests/get_tasks_test.go
+++ api_tests/get_tasks_test.go
@@ -1,1 +1,48 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetTasksByUserID_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Create three lists
+	lists := createLists(e, accessToken, 3)
+
+	// Create three tasks in each list
+	_ = createTasks(e, accessToken, lists, 3)
+
+	// Get tasks
+	tasks := e.GET("/user/tasks").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	// TODO: find out why in the returned data there are no tags!
+
+	printDataToJSON(t, tasks)
+}
