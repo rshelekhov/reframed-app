5a13e0af8923a0dc64e72ed37aa0330799d3cbf4
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-14 18:33:45 +0400

    added tests for GetTask handler

---
+++ api_tests/get_task_test.go
@@ -0,0 +1,88 @@
+package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/constants/key"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/segmentio/ksuid"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetTaskByID_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	task := e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	taskID := task.Value(key.Data).Object().Value(key.TaskID).String().Raw()
+
+	// Get task
+	e.GET("/user/tasks/{task_id}", taskID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
+}
+
+func TestGetTaskByID_NotFound(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	fakeTask := randomFakeTask(true, true, true, true, true, "", "")
+
+	// Create task
+	e.POST("/user/lists/default").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(fakeTask).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object().NotEmpty()
+
+	fakeID := ksuid.New().String()
+
+	e.GET("/user/tasks/{task_id}", fakeID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().Status(http.StatusNotFound)
+}
