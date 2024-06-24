7f676fad7681a68428a6b2f66e4ce2e4e8ba3c14
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-24 23:33:17 +0400

    added test case when tasks not found

--- api_tests/get_upcoming_tasks_test.go
+++ api_tests/get_upcoming_tasks_test.go
@@ -46,9 +46,34 @@ func TestGetUpcomingTasks_HappyPath(t *testing.T) {
 		Status(http.StatusOK).
 		JSON().Object()
 
-	printDataToJSON(t, tasks)
-
 	totalTasksInList := countTasksInGroups(t, tasks, false)
 
 	require.Equal(t, numberOfLists*numberOfTasks, totalTasksInList)
+}
+
+func TestGetUpcomingTasks_NotFound(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Get upcoming tasks
+	e.GET("/user/tasks/upcoming").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object().NotEmpty()
 }
