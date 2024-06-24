d3cecbbf82e95bf5bd45cee35b82c00eb68b1ebd
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-24 22:59:31 +0400

    added test with happy path for get upcoming tasks method

---
+++ api_tests/get_upcoming_tasks_test.go
@@ -0,0 +1,54 @@
+package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetUpcomingTasks_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	numberOfLists := 3
+	numberOfTasks := 3
+
+	// Create three lists
+	lists := createLists(e, accessToken, numberOfLists)
+
+	// Create three tasks in each list
+	_ = createTasksUpd(e, accessToken, upcomingTasks, lists, numberOfTasks)
+
+	// Get upcoming tasks
+	tasks := e.GET("/user/tasks/upcoming").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	printDataToJSON(t, tasks)
+
+	totalTasksInList := countTasksInGroups(t, tasks, false)
+
+	require.Equal(t, numberOfLists*numberOfTasks, totalTasksInList)
+}
