756298bd185aad8bd5cec50a614b5c403214d98f
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-26 23:31:43 +0400

    added test with cursor pagination

--- api_tests/get_tasks_for_someday_test.go
+++ api_tests/get_tasks_for_someday_test.go
@@ -3,6 +3,7 @@ package api_tests
 import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/constants/key"
 	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
 	"github.com/rshelekhov/reframed/internal/model"
 	"github.com/stretchr/testify/require"
@@ -47,11 +48,10 @@ func TestGetTasksForSomeday_HappyPath(t *testing.T) {
 		JSON().Object()
 
 	totalTasks := countTasksInGroups(t, tasks, false)
-
 	require.Equal(t, numberOfLists*numberOfTasks, totalTasks)
 }
 
-func TestGetTasksForSomeday_WithPagination(t *testing.T) {
+func TestGetTasksForSomeday_WithLimit(t *testing.T) {
 	u := url.URL{
 		Scheme: scheme,
 		Host:   host,
@@ -113,10 +113,76 @@ func TestGetTasksForSomeday_WithPagination(t *testing.T) {
 				JSON().Object()
 
 			totalLists := countGroups(t, tasks, false)
-
 			require.Equal(t, tc.expectedLists, totalLists)
 		})
 	}
+}
+
+func TestGetTasksForSomeday_WithPagination(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	numberOfLists := 3
+	numberOfTasks := 3
+
+	// Create three lists
+	lists := createLists(e, accessToken, numberOfLists)
+
+	// Create three tasks in each list
+	_ = createTasks(e, accessToken, somedayTasks, lists, numberOfTasks)
+
+	limit := 1
+
+	// First request to get tasks for someday
+	response := e.GET("/user/tasks/someday").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithQuery(key.Limit, limit).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	printDataToJSON(t, response)
+
+	// Ensure we received tasks
+	totalGroups := countGroups(t, response, false)
+	require.Equal(t, limit, totalGroups)
+
+	// Extract the last list_id from the response
+	lastGroup := response.Value("data").Array().Last().Object()
+	lastListID := lastGroup.Value("list_id").String().Raw()
+
+	t.Log("lastListID", lastListID)
+
+	// Second request to get tasks for someday with afterID set to last list_id from the first response
+	nextResponse := e.GET("/user/tasks/someday").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithQuery(key.Limit, limit).
+		WithQuery(key.Cursor, lastListID).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	printDataToJSON(t, nextResponse)
+
+	// Count the number of task groups (lists) in the second response
+	nextTotalGroups := countGroups(t, nextResponse, false)
+	expectedNextGroups := numberOfLists - limit - 1 // Skip limit and the last group of the first response
+	require.Equal(t, expectedNextGroups, nextTotalGroups)
 }
 
 func TestGetTasksForSomeday_NotFound(t *testing.T) {
