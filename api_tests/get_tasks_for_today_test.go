87733608e59c8037ce68672c90a2f7a550de1cb7
Roman Shelekhov <shelekhoff@gmail.com>
2024-06-27 12:49:56 +0400

    added tests with happy path and fail cases for getTasksForToday method

--- api_tests/get_tasks_for_today_test.go
+++ api_tests/get_tasks_for_today_test.go
@@ -1,1 +1,80 @@
 package api_tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/rshelekhov/reframed/internal/lib/middleware/jwtoken"
+	"github.com/rshelekhov/reframed/internal/model"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetTasksForToday_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	numberOfLists := 3
+	numberOfTasks := 3
+
+	// Create three lists
+	lists := createLists(e, accessToken, numberOfLists)
+
+	// Create three tasks in each list
+	_ = createTasks(e, accessToken, todayTasks, lists, numberOfTasks)
+
+	// Get tasks for today
+	tasks := e.GET("/user/tasks/today").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	totalTasks := countTasksInGroups(t, tasks, false)
+	require.Equal(t, numberOfLists*numberOfTasks, totalTasks)
+}
+
+func TestGetTasksForToday_NotFound(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	r := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := r.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Get tasks for today
+	tasks := e.GET("/user/tasks/today").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
+
+	printDataToJSON(t, tasks)
+}
