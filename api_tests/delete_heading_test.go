20053e361ffe27110bf00701f9008028dc20fce3
Roman Shelekhov <shelekhoff@gmail.com>
2024-08-29 21:09:27 +0400

    added tests

--- api_tests/delete_heading_test.go
+++ api_tests/delete_heading_test.go
@@ -64,3 +64,63 @@ func TestDeleteHeading_HappyPath(t *testing.T) {
 	// Cleanup the SSO gRPC service storage after testing
 	cleanupAuthService(e, user)
 }
+
+func TestDeleteHeading_DoubleDelete(t *testing.T) {
+	u := url.URL{
+		Scheme: scheme,
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	// Register user
+	user := e.POST("/register").
+		WithJSON(model.UserRequestData{
+			Email:    gofakeit.Email(),
+			Password: randomFakePassword(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	accessToken := user.Value(jwtoken.AccessTokenKey).String().Raw()
+
+	// Create list
+	list := e.POST("/user/lists/").
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.ListRequestData{
+			Title: gofakeit.Word(),
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	listID := list.Value(key.Data).Object().Value(key.ListID).String().Raw()
+
+	// Create heading
+	heading := e.POST("/user/lists/{list_id}/headings/", listID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		WithJSON(model.HeadingRequestData{
+			Title:  gofakeit.Word(),
+			ListID: listID,
+		}).
+		Expect().
+		Status(http.StatusCreated).
+		JSON().Object()
+
+	headingID := heading.Value(key.Data).Object().Value(key.HeadingID).String().Raw()
+
+	// Delete heading
+	e.DELETE("/user/lists/{list_id}/headings/{heading_id}", listID, headingID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusOK)
+
+	// Try to delete heading again
+	e.DELETE("/user/lists/{list_id}/headings/{heading_id}", listID, headingID).
+		WithHeader("Authorization", "Bearer "+accessToken).
+		Expect().
+		Status(http.StatusNotFound)
+
+	// Cleanup the SSO gRPC service storage after testing
+	cleanupAuthService(e, user)
+}
